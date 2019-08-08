bbe43a774d402783d7fbb02236c5a504ba947023
Adebimpe Odefunsho <adebimpeodefunsho@gmail.com>
2019-08-08 12:35:22 +0100

    Update Generating-random-sample.py

--- Generating-random-sample.py
+++ Generating-random-sample.py
@@ -9,6 +9,7 @@ sample1 = np.random.choice(students, 10, replace=True)
 # getting 10,000 proportion of the above sampling distribution
 sample_props = []
 for _ in range(10000):
+#this generates 5 draws from the students numpy array
     sample = np.random.choice(students, 5, replace=True)
     sample_proppp.append(sample.mean())
 sample_props = np.array(sample_props)
