baac69354a2662118c4d35a9554abb7c9167ee6d
Adebimpe Odefunsho <adebimpeodefunsho@gmail.com>
2019-08-10 11:41:26 +0100

    Update Generating-random-sample.py

--- Generating-random-sample.py
+++ Generating-random-sample.py
@@ -3,13 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 np.random.seed(42)
 
-students = np.array([1,0,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0])
-#simulating 10 draws from student array
-sample1 = np.random.choice(students, 10, replace=True)
-# getting 10,000 proportion of the above sampling distribution
+fraud = np.array([1,0,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0])
+#simulating 10 draws from fraud array
+sample1 = np.random.choice(fraud, 10, replace=True)
+# getting 10,000 iterations of the above sampling distribution
 sample_props = []
 for _ in range(10000):
-#this generates 5 draws from the students numpy array
-    sample = np.random.choice(students, 5, replace=True)
-    sample_proppp.append(sample.mean())
-sample_props = np.array(sample_props)
+#this generates 5 draws from the fraud numpy array
+    sample = np.random.choice(fraud, 5, replace=True)
+    sample_props.append(sample.mean())
+    sample_props = np.array(sample_props)
